09e5049ba2b05fb541f6c5ef339803bd724ac21e
yaohuiming <yaohuiming789@gmail.com>
2022-12-28 00:18:17 +0800

    add some comment for ProxyService

--- src/service/proxy_service.rs
+++ src/service/proxy_service.rs
@@ -38,6 +38,7 @@ struct Router {
 }
 
 impl Router {
+    /// 更新路由信息
     fn update_router(&mut self, redis_node_infos: Vec<RedisNodeInfo>) -> Result<()> {
         for redis_node_info in redis_node_infos {
             let slot_range = redis_node_info
@@ -57,6 +58,7 @@ impl Router {
         Ok(())
     }
 
+    /// 拿到与key对应的redis节点之间的grpc channel
     fn get_redis_channel_by_key(&self, key: impl AsRef<str>) -> Result<Channel> {
         let crc = Crc::<u16>::new(&CRC_16_IBM_SDLC);
         let slot_index = crc.checksum(key.as_ref().as_bytes()) as usize % SLOTS_LENGTH;
@@ -66,18 +68,21 @@ impl Router {
             .cloned()
     }
 
+    /// 转发客户端发来的set请求到对应的redis节点
     async fn forward_set(&self, request: SetRequest) -> Result<SetResponse> {
         let channel = self.get_redis_channel_by_key(&request.key)?;
         let mut client = RedisServiceClient::new(channel);
         Ok(client.set(Request::new(request)).await?.into_inner())
     }
 
+    /// 转发客户端发来的get请求到对应的redis节点
     async fn forward_get(&self, request: GetRequest) -> Result<GetResponse> {
         let channel = self.get_redis_channel_by_key(&request.key)?;
         let mut client = RedisServiceClient::new(channel);
         Ok(client.get(Request::new(request)).await?.into_inner())
     }
 
+    /// 转发客户端发来的remove请求到对应的redis节点
     async fn forward_remove(&self, request: RemoveRequest) -> Result<RemoveResponse> {
         let channel = self.get_redis_channel_by_key(&request.key)?;
         let mut client = RedisServiceClient::new(channel);
@@ -93,11 +98,13 @@ struct ProxyNode {
 
 #[async_trait]
 impl ProxyManageService for ProxyNode {
+    /// 将该代理节点设置为过期状态，设置为过期状态的代理节点不向客户端提供服务
     async fn expired(&self, _: Request<()>) -> Result<Response<()>, Status> {
         *self.state.write().await = State::Expired;
         Ok(Response::new(()))
     }
 
+    /// 更新代理节点的路由信息，并将状态更新为Sync，Sync状态的proxy节点可以正常响应客户端的请求
     async fn sync(&self, request: Request<SyncRequest>) -> Result<Response<()>, Status> {
         let mut state_write_guard = self.state.write().await;
         match *state_write_guard {
@@ -117,6 +124,7 @@ impl ProxyManageService for ProxyNode {
         }
     }
 
+    /// 获得当前proxy节点的状态
     async fn get_state(&self, _: Request<()>) -> Result<Response<GetStateResponse>, tonic::Status> {
         Ok(Response::new(GetStateResponse {
             state: *self.state.read().await as i32,
